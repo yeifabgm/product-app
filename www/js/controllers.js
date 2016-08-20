ac24dbb6adafb1721217d90fe1c9e6cec96e547d
jonathanCarvajalG <joan_tom@hotmail.com>
2016-08-19 21:46:46 -0500

    add libreria resource

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['app.services'])
   
 .controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -8,11 +8,21 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('ingresoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('ingresoCtrl', ['$scope', '$stateParams','productService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  $scope.login = function(){
+
+    var data = {
+      ''
+    };
+    productService.user_login.post(data,function (data){
+
+
+    })
+  }
 
 }])
    
